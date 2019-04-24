dd1bfa0996d8d68de98dcbec3fa0bc4c00044d08
gmimixenia <gmimixenia@gmail.com>
2019-04-24 16:19:44 +0300

    Branch2 code

--- 1.cpp
+++ 1.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 
 using namespace std;
-
+void Part2();
 int main()
 {
 	cout << "Это начало кода" << endl;
+	Part2();
 	return 0;
+}
+
+void Part2()
+{
+	cout << "Это для ветки номер 2" << endl;
 }
